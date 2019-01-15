414d4beece0f5ac444cb64ad37aa4b95d1ab169d
KevinKnightIDEXX <kevin-knight@idexx.com>
2019-01-15 17:13:27 -0600

    Adding programs from earlier in workshop to analyze packages - mumpsimus

---
+++ R/90_all-progs.R
@@ -0,0 +1,12 @@
+
+source(here::here("R","01_write-installed-packages.R"))
+
+
+source(here::here("R","02_wrangle-packages.R"))
+
+
+source(here::here("R","03_barchart-packages-built.R"))
+
+
+#better to use call R function in a package - allows to run in separate R sessions.
+
